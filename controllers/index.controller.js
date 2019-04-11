2f25b8507bb7c7ce757d9f386a1ca2764735c113
Thanh Quy Vo <vtquy98@gmail.com>
2019-04-11 12:28:31 +0700

    pagination at index page (not done)

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -4,30 +4,35 @@ var post = require('../models/post.model');
 module.exports.index = function(req, res) {
 
     
+    var page = parseInt((req.query.page)) || 1; 
+	var postPerPage = 4;
+
+	var start = ((page - 1) * postPerPage);
+	var end = page * postPerPage;
 
 
-    post.find().sort({_id: -1}).then(function (post) {
-        // if(post[1].description)
-        console.log(post[1].description.length);
+	post.find().sort({_id: -1}).then(function(p){
         var string1;
         var string2;
-        if(post[1].description.length > 200){
-            string1 = post[1].description.slice(0, 115) + "...";
+        if(p[1].description.length > 200){
+            string1 = p[1].description.slice(0, 115) + "...";
         }
-        if(post[2].description.length > 200){
-            string2 = post[2].description.slice(0, 115) + "...";
+        if(p[2].description.length > 200){
+            string2 = p[2].description.slice(0, 115) + "...";
         }
 
-        res.render('index', {
-            posts: post.slice(3,post.length),
-            oneNewestPost: post[0],
-            twoNewestPost1: post[1],
+		res.render('index', {
+			posts: p.slice(start, end),
+			pages: Math.ceil(p.length/postPerPage),
+            currentPage: page,
+
+            oneNewestPost: p[0],
+            twoNewestPost1: p[1],
             post1Des: string1, 
-            twoNewestPost2: post[2],
-            post2Des: string2,                 
-        })
-  
-    })
+            twoNewestPost2: p[2],
+            post2Des: string2,  
+		});
+	})
 }
 
 module.exports.view = function (req, res) {
